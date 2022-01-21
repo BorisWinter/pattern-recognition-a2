378259d9d870ff674b0398cc0bb15e4bce0fe451
Brandsma <a.brandsma@protonmail.com>
2022-01-21 16:24:15 +0100

    Fixed FT feature selection

--- feature_selection/fourier_transform.py
+++ feature_selection/fourier_transform.py
@@ -1,10 +1,13 @@
 import numpy as np
-
+import pandas as pd
 def ft_on_img_data(img_data):
-    print(len(img_data))
-    f = np.fft.fft2(img_data)
-    fshift = np.fft.fftshift(f)
-    return 20*np.log(np.abs(fshift))
+    new_df = pd.DataFrame(index=np.arange(len(img_data)), columns=np.arange(len(img_data.iloc[0])))
+    for idx, img in img_data.iterrows():
+        print(f"processing image #{idx+1} out of {len(img_data)}", end="\r")
+        f = np.fft.fft(img)
+        fshift = np.fft.fftshift(f)
+        new_df.append(pd.Series(20*np.log(np.abs(fshift))), ignore_index=True)
+    return new_df
 
 
 def ft_on_num_data(num_data):
