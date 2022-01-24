45720ac0ad0a7f4427cdac10e7c26edfecc219b2
BorisWinter <borisbenjaminwinter@gmail.com>
2022-01-24 16:40:32 +0100

    Added cross validation functionality for knn

--- classification/knn.py
+++ classification/knn.py
@@ -1,19 +1,44 @@
 # Imports
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.metrics import accuracy_score, f1_score
 
 
-def knn(train_data, train_labels, test_data, k):
+
+def knn(train_data, train_labels, test_data, test_labels, k):
     """
-    Performs kNN on the given data.
+    Performs k-NN on the given data.
     """
 
     neighbours = KNeighborsClassifier(n_neighbors=k)
     neighbours.fit(train_data, train_labels)
+    pred_labels = neighbours.predict(test_data)
 
-    return neighbours.predict(test_data)
+    acc = accuracy_score(test_labels, pred_labels)
+    f1 = f1_score(test_labels, pred_labels, average="weighted")
+
+    return acc, f1, pred_labels
+
+
+def knn_cross_validation(data, labels, k):
+    """
+    Evaluates k-NN using 10-fold cross validation on the given data.
+    """
+
+    # Cross validation settings
+    kf = StratifiedKFold(n_splits = 10, random_state = 1, shuffle=True)
+
+    # Create the k-NN classifier
+    clf = KNeighborsClassifier(n_neighbors=k)
+
+    result = cross_validate(clf, data, labels, cv=kf, scoring=["accuracy", "f1_weighted"])
+    test_acc = result["test_accuracy"].mean()
+    test_f1 = result["test_f1_weighted"].mean()
+
+    return test_acc, test_f1
 
 
 def knn_gridsearch(data, labels, k_range, n_splits = 10):
@@ -26,7 +51,7 @@ def knn_gridsearch(data, labels, k_range, n_splits = 10):
     knn_results = pd.DataFrame([], columns = ["param_n_neighbors", "mean_train_score", "mean_test_score"])
 
     # Cross validation settings
-    kf = KFold(n_splits = n_splits, random_state = 1, shuffle=True)
+    kf = StratifiedKFold(n_splits = n_splits, random_state = 1, shuffle=True)
 
     # Define the model
     knn_model = KNeighborsClassifier()
