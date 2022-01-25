8dde59d36a104e76e5faef84d73442d3aebc6eb5
BorisWinter <borisbenjaminwinter@gmail.com>
2022-01-25 15:23:44 +0100

    Added t-SNE visualization functionality

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -20,6 +20,7 @@ def create_conf_matrix(labels, pred_labels, title = "Confusion Matrix"):
     ax.set_xlabel("Predicted class", fontsize=30)
     ax.set_ylabel("True class", fontsize=30)
     ax.set_title(title, fontsize=40, pad=20)
+    
 
 def simple_pca_line_plot(y, title="Line Plot", ylabel="y"):
     """
@@ -32,3 +33,25 @@ def simple_pca_line_plot(y, title="Line Plot", ylabel="y"):
     ax.set_xlabel("Number of principal components", fontsize=30)
     ax.set_ylabel(ylabel, fontsize=30)
     ax.set_title(title, fontsize=40, pad=20)
+
+
+def plot_tsne_num(num_data_embedded, num_labels, title, xlabel, ylabel):
+    fig, ax = plt.subplots(figsize=(15,10))
+    num_labels[1].name = "Class"
+    ax = sns.scatterplot(num_data_embedded[:,0], num_data_embedded[:,1], hue=num_labels[1])
+    ax.tick_params(labelsize=20)
+    ax.set_xlabel(xlabel, fontsize=30)
+    ax.set_ylabel(ylabel, fontsize=30)
+    ax.set_title(title, fontsize=40, pad=20)
+    plt.legend(fontsize='xx-large', title_fontsize='40')
+
+
+def plot_tsne_img(img_data_embedded, img_labels, title, xlabel, ylabel):
+    fig, ax = plt.subplots(figsize=(15,10))
+    ax = sns.scatterplot(img_data_embedded[:,0], img_data_embedded[:,1], hue=img_labels)
+    ax.tick_params(labelsize=20)
+    ax.set_xlabel(xlabel, fontsize=30)
+    ax.set_ylabel(ylabel, fontsize=30)
+    ax.set_title(title, fontsize=40, pad=20)
+    plt.legend(fontsize='xx-large', title_fontsize='40')
+
