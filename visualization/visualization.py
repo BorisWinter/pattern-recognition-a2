770fe93e1938e56a2fe6563c9f95cc9e5dcdbfa0
BorisWinter <borisbenjaminwinter@gmail.com>
2022-01-25 09:40:08 +0100

    Fixed bug where completely 0 columns don't show

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -1,17 +1,24 @@
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 def create_conf_matrix(labels, pred_labels):
+    """
+    Takes true labels and predicted labels.
+    Returns a heatmap of the corresponding confusion matrix.
+    """
+
+    classes = np.unique(np.append(labels, pred_labels))
     conf_matrix = confusion_matrix(labels, pred_labels)
-    # classes = labels.unique()
-    classes = list(set(list(labels)))
+    return_matrix = conf_matrix.astype('float') / conf_matrix.sum(axis=1)[:, np.newaxis]
+
     fig, ax = plt.subplots(figsize=(10,10))
     sns. set(font_scale=1.8)
-    ax = sns.heatmap(conf_matrix/sum(conf_matrix), annot=True, fmt=".2%", linewidth=.5, cbar=False, xticklabels=classes, yticklabels=classes)
+    ax = sns.heatmap(return_matrix, annot=True, fmt=".2%", linewidth=.5, cbar=False, xticklabels=classes, yticklabels=classes)
     ax.tick_params(labelsize=20)
     ax.set_xlabel("Predicted class", fontsize=30)
     ax.set_ylabel("True class", fontsize=30)
     ax.set_title("Confusion matrix", fontsize=40)
 
-    return ax
+    return return_matrix
